df652462fc1a002b432b46204fd576a1e2583cf9
Maria Camila Calle Agudelo <mcalle1@eafit.edu.co>
2019-05-08 14:55:36 -0500

    Limpiando

--- routes/crearPartida.js
+++ routes/crearPartida.js
@@ -12,7 +12,7 @@ var gestPer = require("../Middelware/gestionPersonaje");
 var partidas = new Map();
 
 router.get("/", (req, res) => {
-    res.send("Server Villa Innovadora ");
+    res.send("Server Villa Innovadora Demo ");
 }
 )
 
