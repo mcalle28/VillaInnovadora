7175317fbd7f9bd5ffebb3b028ad64193df2ab71
Ricardo R. Azopardo <rrazopardc@eafit.edu.co>
2019-05-24 13:19:12 -0500

    header.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     if ('OPTIONS' == req.method) {
